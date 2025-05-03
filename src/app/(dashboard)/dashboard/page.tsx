415ef28a3e4a540efae586bd10ded3315d2c6923
JojayD <professional.josephd@gmail.com>
2025-05-03 13:56:04 -0700

    removed recording button

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -138,12 +138,12 @@ export default function Dashboard() {
 						/>
 					</div>
 					<div>
-						<Button
+						{/* <Button
 							variant='outline'
 							className='bg-orange-500 text-white hover:bg-orange-700 mr-2'
 						>
 							Recordings
-						</Button>
+						</Button> */}
 						<Button
 							onClick={() => setShowUpdates(!showUpdates)}
 							variant='outline'
